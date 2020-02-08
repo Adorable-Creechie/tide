a2e3bb91c347cecc8bc69c8f6548b88d205b8b17
Anonymous <>
2020-02-08 23:15:21 +0100

    fix: myoplay.live

--- sources/myoplaylive.py
+++ sources/myoplaylive.py
@@ -1,9 +1,13 @@
 """
 myoplay.live
 
-method:
+method 1:
 search for iframe https://angelthump.com/{id}/embed
 m3u8 = https://video-cdn.angelthump.com/hls/{id}.m3u8
+
+method 2:
+search for iframe.contains "embed"
+xyzembed common
 """
 
 NAME = "myoplay.live"
@@ -27,6 +31,7 @@ except Exception as e:
 
 import re
 import urllib
+from bs4 import BeautifulSoup
 
 def can_handle(url):
     p_url = parse_url(url)
@@ -39,14 +44,33 @@ def root(url):
     add_items(urls, ref_url, PLUGIN)
 
 def get_urls(url):
+    p_url = parse_url(url)
     headers = header_random_agent()
     html = http_get(url, headers=headers)
-    angel_id = re.search(r"angelthump.com/(.*?)/embed", html.text).group(1)
+    soup = BeautifulSoup(html.text, "html.parser")
+    angel = re.search(r"angelthump.com/(.*?)/embed", html.text)
     headers.update({
         "Referer": url,
         "Origin": url,
     })
-    return [M3U8_URL % angel_id]
+    if angel:
+        angel_id = angel.group(1)
+        return [M3U8_URL % angel_id]
+    else:
+        xyz = soup.find(allowfullscreen="true")
+        xyz_url = "%s:%s" % (p_url.scheme, xyz.get("src"))
+        html = http_get(xyz_url, headers=headers)
+        return xyzembed(html.text)
+
+# modification of wstream algo
+def xyzembed(html):
+    eval_f = re.search(r"<script>(eval.*?)</script>", html, re.MULTILINE | re.DOTALL).group(1)
+    f = re.search(r"eval\((.*)\)", eval_f, re.MULTILINE | re.DOTALL).group(1)
+    p = re.search(r"bestfit\|(.*)\|parentId", f).group(1).split('|')
+    m3u8_url = "%s://%s.%s.%s:%s/%s/%s.%s?s=%s&e=%s" % (
+        p[4], p[5], p[6], p[7], p[8], p[2], p[9], p[11], p[0], p[-1]
+    )
+    return m3u8_url
 
 if __name__ == "__main__":
     def test(url):
